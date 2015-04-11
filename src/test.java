b0fae68582a4a4fa377f2367f9f2ddc1ae206f45
Brockify <brdamico@csumb.edu>
2015-04-11 03:58:49 +0000

    Definte fixtures

--- src/test.java
+++ src/test.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import sun.nio.cs.Surrogate;
 
@@ -10,6 +11,7 @@ import static org.junit.Assert.assertEquals;
  * Created by vagrant on 4/10/15.
  */
 public class test {
+    private List<String> names;
 
     @Test
     public void findGroup() throws Exception {
@@ -24,5 +26,21 @@ public class test {
         String name = "a";
         ArrayList<String>groups = generator.createGroup(name);
         assertEquals(groups.contains(name), true);
+    }
+    @Before
+    public void setUp() {
+        this.names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+    }
+
+    @Test
+    public void groupSize() {
+        grouppicker generator = new grouppicker(this.names);
+        System.out.println(this.names.size());
     }
 }
